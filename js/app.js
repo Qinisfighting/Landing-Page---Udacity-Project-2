115fe665fcb0fb6ae350a42b1bf8b95342d69209
Qin <95076245+Qinisfighting@users.noreply.github.com>
2022-03-14 19:52:19 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -140,8 +140,13 @@ function topFunction() {
   document.body.scrollTo({
     top: 0,
     behavior: "smooth"
-  });
+  }); // For Safari
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  }); // For Chrome, Firefox, IE and Opera
 }
+
 /**
  * End Main Functions
  */
